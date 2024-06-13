be40cbcb4f64b835ed9418673ce2df30bada0e25
Gabriel Fellipe Marquardt <gfellipe@weg.net>
2024-06-13 14:23:40 -0300

    feature: first commit

---
+++ src/main/java/com/coolors/coolors/CoolorsApplication.java
@@ -0,0 +1,13 @@
+package com.coolors.coolors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoolorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoolorsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coolors/coolors/CoolorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coolors.coolors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoolorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
